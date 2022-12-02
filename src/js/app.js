48282a953db679ffd84c2564b34b26d1285dcda1
Valentin Vladimirov <valentinvladimirov@abv.bg>
2022-12-02 21:18:46 +0200

    bug fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,13 @@
-import "../scss/app.scss";
+//import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
-  // This block will be executed once the page is loaded and ready
+    // This block will be executed once the page is loaded and ready
+    setTimeout(function () {
+        document.title = "One new message";
+    }, 3000);
 
-  const button = document.querySelector(".button");
-  button.addEventListener("click", () => {
-    alert("💣");
-  });
+    const button = document.querySelector(".button");
+    button.addEventListener("click", () => {
+        alert("💣");
+    });
 });
